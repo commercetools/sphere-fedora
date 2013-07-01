a36e688546a7386cc8dc7d02eb519eed938a2878
Laura Luiz <laura.luiz@commercetools.de>
2013-07-01 13:19:43 +0200

    Fix save context function to save only those successful calls.

--- app/controllers/actions/SaveContext.java
+++ app/controllers/actions/SaveContext.java
@@ -1,5 +1,6 @@
 package controllers.actions;
 
+import play.core.j.JavaResultExtractor;
 import play.mvc.Action;
 import play.mvc.Http;
 import play.mvc.Result;
@@ -10,10 +11,14 @@ public class SaveContext extends Action.Simple {
     * Saves the current URL in session to be used later as a return URL
     * */
     public Result call(Http.Context ctx) throws Throwable {
-        // Before
-        ctx.session().put("returnUrl", ctx.request().uri());
-
         // Call
-        return delegate.call(ctx);
+        Result result = delegate.call(ctx);
+
+        // After
+        if (JavaResultExtractor.getStatus(result) == 200) {
+            ctx.session().put("returnUrl", ctx.request().uri());
+        }
+
+        return result;
     }
 }
