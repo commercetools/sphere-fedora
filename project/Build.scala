be6690dc49547c7c6a5e7be61132e62340ab376b
Laura Luiz <laura.luiz@commercetools.de>
2013-07-30 18:00:39 +0200

    Update to SDK 0.38.0.

--- project/Build.scala
+++ project/Build.scala
@@ -31,7 +31,7 @@ object ApplicationBuild extends Build {
 
     object Libs {
         val appDependencies = Seq(
-            "io.sphere"     %%  "sphere-play-sdk"   %   "0.36.1",
+            "io.sphere"     %%  "sphere-play-sdk"   %   "0.38.0",
             "javax.mail"    %   "mail"              %   "1.4.7",
             "org.jsoup"     %   "jsoup"             %   "1.7.1"
         )
