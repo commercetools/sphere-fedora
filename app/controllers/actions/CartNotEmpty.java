6d6e9c6c1e809a7b04d6b60c1d124c375668349a
schleichardt <michael.schleichardt@commercetools.de>
2014-06-20 14:22:48 +0200

    Migration to Play 2.2.3, SBT 0.13.5 and Java 7.

--- app/controllers/actions/CartNotEmpty.java
+++ app/controllers/actions/CartNotEmpty.java
@@ -1,8 +1,9 @@
 package controllers.actions;
 
+import play.libs.F;
 import play.mvc.Action;
 import play.mvc.Http;
-import play.mvc.Result;
+import play.mvc.SimpleResult;
 import sphere.Sphere;
 
 public class CartNotEmpty extends Action.Simple {
@@ -10,13 +11,10 @@ public class CartNotEmpty extends Action.Simple {
     /*
     * Checks whether the current cart is empty
     * */
-    public Result call(Http.Context ctx) throws Throwable {
-        // Before
+    public F.Promise<SimpleResult> call(Http.Context ctx) throws Throwable {
         if (Sphere.getInstance().currentCart().getQuantity() < 1) {
-            return redirect(ctx.session().get("returnUrl"));
+            return F.Promise.pure(redirect(ctx.session().get("returnUrl")));
         }
-
-        // Call
         return delegate.call(ctx);
     }
 }
---
+++ build.sbt
@@ -0,0 +1,30 @@
+import play.Project._
+
+organization := "io.sphere.shop"
+
+name := "sphere-fedora"
+
+version := "1.0-SNAPSHOT"
+
+playJavaSettings
+
+libraryDependencies ++= Seq(
+  javaCore,
+  javaJdbc,
+  "io.sphere" %% "sphere-play-sdk" % "0.59.0" withSources(),
+  "javax.mail" % "mail" % "1.4.7",
+  "org.jsoup" % "jsoup" % "1.7.1",
+  "org.mockito" % "mockito-all" % "1.9.5" % "test"
+)
+
+lessEntryPoints := baseDirectory.value / "app" / "assets" / "stylesheets" * "*.less"
+
+templatesImport ++= Seq(
+  "utils.ViewHelper._",
+  "forms._",
+  "io.sphere.client.model._",
+  "io.sphere.client.filters._",
+  "io.sphere.client.shop.model._"
+)
+
+javacOptions ++= Seq("-source", "1.7", "-target", "1.7")
