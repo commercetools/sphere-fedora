d092410ea11feed190980611111406fda2abbef4
Laura Luiz <laura.luiz@commercetools.de>
2014-09-02 19:14:39 +0200

    Implement category service.

---
+++ app/services/CategoryService.java
@@ -0,0 +1,34 @@
+package services;
+
+import com.google.common.base.Optional;
+import models.ShopCategory;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    /**
+     * Gets the category with the provided ID.
+     * @param categoryId internal identifier of the category.
+     * @return the category with this ID, or absent if it does not exist.
+     */
+    Optional<ShopCategory> getById(String categoryId);
+
+    /**
+     * Gets the category with the provided slug.
+     * @param categorySlug external and human-readable identifier of the category.
+     * @return the category with this slug, or absent if it does not exist.
+     */
+    Optional<ShopCategory> getBySlug(String categorySlug);
+
+    /**
+     * Gets the root categories of the project.
+     * @return the list of root categories.
+     */
+    List<ShopCategory> getRoots();
+
+    /**
+     * Fetches all categories from the backend again, refreshing the cached categories with latest version.
+     */
+    void refresh();
+}
---
+++ app/services/CategoryServiceImpl.java
@@ -0,0 +1,53 @@
+package services;
+
+import com.google.common.base.Optional;
+import io.sphere.client.shop.model.Category;
+import models.ShopCategory;
+import sphere.Sphere;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService {
+    private final Sphere sphere;
+
+    public CategoryServiceImpl(final Sphere sphere) {
+        this.sphere = sphere;
+    }
+
+    @Override
+    public Optional<ShopCategory> getById(final String categoryId) {
+        Category category = sphere.categories().getById(categoryId);
+        if (category != null) {
+            ShopCategory fetchedCategory = ShopCategory.of(category);
+            return Optional.of(fetchedCategory);
+        } else {
+            return Optional.absent();
+        }
+    }
+
+    @Override
+    public Optional<ShopCategory> getBySlug(final String categorySlug) {
+        Category category = sphere.categories().getBySlug(categorySlug);
+        if (category != null) {
+            ShopCategory fetchedCategory = ShopCategory.of(category);
+            return Optional.of(fetchedCategory);
+        } else {
+            return Optional.absent();
+        }
+    }
+
+    @Override
+    public List<ShopCategory> getRoots() {
+        List<ShopCategory> roots = new ArrayList<>();
+        for (Category root : sphere.categories().getRoots()) {
+            roots.add(ShopCategory.of(root));
+        }
+        return roots;
+    }
+
+    @Override
+    public void refresh() {
+        sphere.categories().rebuildAsync();
+    }
+}
