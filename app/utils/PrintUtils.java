8ad9f7dab8e6064a12d309f6630e33343f809e18
Laura Luiz <laura.luiz@commercetools.de>
2014-09-04 19:27:12 +0200

    Add draft print methods for parameters.

--- app/utils/PrintUtils.java
+++ app/utils/PrintUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import io.sphere.client.model.Money;
+import play.i18n.Messages;
 
 import java.text.NumberFormat;
 import java.util.Currency;
@@ -19,4 +20,30 @@ public final class PrintUtils {
         return Currency.getInstance(currencyCode).getSymbol(locale);
     }
 
+    public static String printSortOption(String sortOption) {
+        String key = "pop.sort.option." + sortOption;
+        if (Messages.isDefined(key)) {
+            return Messages.get(key);
+        } else {
+            return sortOption;
+        }
+    }
+
+    public static String printPriceOption(String priceOption) {
+        String key = "pop.price.option." + priceOption;
+        if (Messages.isDefined(key)) {
+            return Messages.get(key);
+        } else {
+            return priceOption;
+        }
+    }
+
+    public static String printAmountOption(String amountOption) {
+        String key = "pop.amount.option." + amountOption;
+        if (Messages.isDefined(key)) {
+            return Messages.get(key);
+        } else {
+            return amountOption;
+        }
+    }
 }
