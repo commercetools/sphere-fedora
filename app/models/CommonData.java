ce27f07bfdc05196b049f22eede755bf1dcc1818
Laura Luiz <laura.luiz@commercetools.de>
2014-09-05 19:48:29 +0200

    Add convenient methods to generate routes.

--- app/models/CommonData.java
+++ app/models/CommonData.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +8,6 @@ import com.google.common.base.Optional;
 import controllers.urls.ShopCall;
 import controllers.urls.ShopRoutes;
 import play.i18n.Lang;
-import play.mvc.Call;
 
 /**
  * A container for stuff needed in almost every template.
@@ -19,15 +19,17 @@ public class CommonData {
     private final Optional<ShopCategory> currentCategory;
     private final Optional<ShopProduct> currentProduct;
     private final ShopRoutes shopRoutes;
+    private final RequestParameters requestParameters;
 
-    CommonData(UserContext userContext, List<Lang> availableLang, ShopRoutes shopRoutes, List<ShopCategory> rootCategories,
-               Optional<ShopCategory> currentCategory, Optional<ShopProduct> currentProduct) {
+    CommonData(UserContext userContext, List<Lang> availableLang, ShopRoutes shopRoutes, RequestParameters requestParameters,
+               List<ShopCategory> rootCategories, Optional<ShopCategory> currentCategory, Optional<ShopProduct> currentProduct) {
         this.userContext = userContext;
         this.availableLang = availableLang;
         this.rootCategories = rootCategories;
         this.currentCategory = currentCategory;
         this.currentProduct = currentProduct;
         this.shopRoutes = shopRoutes;
+        this.requestParameters = requestParameters;
     }
 
     public UserContext context() {
@@ -43,7 +45,29 @@ public class CommonData {
     }
 
     public Map<Lang, ShopCall> localizedRoutes() {
-        return shopRoutes.all(currentCategory, currentProduct);
+        if (currentProduct.isPresent()) {
+            return shopRoutes.products().all(currentProduct.get(), currentCategory);
+        } else if (currentCategory.isPresent()) {
+            return shopRoutes.categories().all(currentCategory.get(), requestParameters);
+        } else {
+            return shopRoutes.any().all(requestParameters);
+        }
+    }
+
+    public ShopCall route() {
+        return shopRoutes.any().get(requestParameters);
+    }
+
+    public ShopCall filterCategoryRoute(ShopCategory category) {
+        return shopRoutes.categories().get(category, requestParameters);
+    }
+
+    public ShopCall categoryRoute(ShopCategory category) {
+        return shopRoutes.categories().get(category, RequestParameters.empty());
+    }
+
+    public ShopCall productRoute(ShopProduct product, Optional<ShopCategory> category) {
+        return shopRoutes.products().get(product, category);
     }
 
     public Optional<ShopCategory> currentCategory() {
