e376cd0e02553b4ed7e71898728044c9b984f608
schleichardt <michael.schleichardt@commercetools.de>
2014-09-05 15:27:51 +0200

    Add menu to index page.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,15 +6,23 @@ import io.sphere.client.model.SearchResult;
 import io.sphere.client.shop.model.Product;
 import play.mvc.Result;
 import play.mvc.With;
-import sphere.ShopController;
+import services.CartService;
+import services.CategoryService;
+import services.CustomerService;
+import services.ProductService;
 import views.html.index;
 
-public class Application extends ShopController {
+public class Application extends BaseController {
+
+    public Application(final CategoryService categoryService, final ProductService productService,
+                       final CartService cartService, final CustomerService customerService) {
+        super(categoryService, productService, cartService, customerService);
+    }
 
     @With(SaveContext.class)
-    public static Result home() {
+    public Result home() {
         SearchResult<Product> searchResultNew = sphere().products().all(lang().toLocale()).sort(ProductSort.price.desc).fetch();
         SearchResult<Product> searchResultOffer = sphere().products().all(lang().toLocale()).sort(ProductSort.price.asc).fetch();
-        return ok(index.render(searchResultNew, searchResultOffer));
+        return ok(index.render(data().build(), searchResultNew, searchResultOffer));
     }
 }
