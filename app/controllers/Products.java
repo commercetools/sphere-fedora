ba4902b9bb4e1b10a56e3befec73a722620c1fe6
schleichardt <michael.schleichardt@commercetools.de>
2014-09-05 18:25:32 +0200

    Display orders on my account page.

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -9,24 +9,33 @@ import io.sphere.client.shop.model.Product;
 import io.sphere.client.shop.model.Variant;
 import play.mvc.Result;
 import play.mvc.With;
-import sphere.SearchRequest;
 import sphere.ShopController;
 import views.html.productDetail;
 
 import java.util.Collections;
-import java.util.Locale;
 
 
 @With(SaveContext.class)
 public class Products extends ShopController {
 
     public static Result select(String productSlug, int variantId) {
-        // Case invalid product
         Product product = sphere().products().bySlug(lang().toLocale(), productSlug).fetch().orNull();
         if (product == null) {
             return notFound("Product not found: " + productSlug);
         }
-        // Case valid select product
+        return renderProduct(variantId, product);
+    }
+
+    //TODO this should not be indexed by search engines
+    public static Result selectById(String productId, int variantId) {
+        Product product = sphere().products().byId(productId).fetch().orNull();
+        if (product == null) {
+            return notFound("Product not found: " + productId);
+        }
+        return renderProduct(variantId, product);
+    }
+
+    private static Result renderProduct(int variantId, Product product) {
         Variant variant = product.getVariants().byId(variantId).or(product.getMasterVariant());
         Category category = product.getCategories().get(0);
         FilterExpression categoryFilter = new FilterExpressions.Categories(Collections.singletonList(category));
