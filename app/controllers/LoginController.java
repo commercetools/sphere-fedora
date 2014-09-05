125a0f5deb750a0f2834ca57022cdca2104b924f
schleichardt <michael.schleichardt@commercetools.de>
2014-09-05 14:39:08 +0200

    Implement login the old way.

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -6,6 +6,7 @@ import io.sphere.client.exceptions.EmailAlreadyInUseException;
 import play.data.Form;
 import play.mvc.Result;
 import sphere.ShopController;
+import views.html.loginView;
 
 import static play.data.Form.form;
 
@@ -14,13 +15,18 @@ public class LoginController extends ShopController {
     final static Form<LogIn> logInForm = form(LogIn.class);
     final static Form<SignUp> signUpForm = form(SignUp.class);
 
-
-
+    /**
+     * Shows a form to login as customer to this shop.
+     * If a session already exists, it will be destroyed.
+     *
+     * @return login form page
+     */
     public static Result signIn() {
         if (sphere().isLoggedIn()) {
             sphere().logout();
         }
-        return ok();
+        //TODO bind from request if errors present
+        return ok(loginView.render(logInForm));
     }
 
     public static Result showSignUp() {
@@ -48,25 +54,27 @@ public class LoginController extends ShopController {
         return redirect(routes.CustomerController.show());
     }
 
+    /**
+     * Handles the login form submission.
+     * @return the shop homepage on success or the login form on error.
+     */
     public static Result logIn() {
-        Form<LogIn> form = logInForm.bindFromRequest();
-        // Case missing or invalid form data
-        if (form.hasErrors()) {
-            flash("error", "Login form contains missing or invalid data");
-            return badRequest();
+        Form<LogIn> filledForm = logInForm.bindFromRequest();
+        if (filledForm.hasErrors()) {
+            flash("error", "Login form contains missing or invalid data.");
+            return badRequest(loginView.render(filledForm));
+        } else {
+            LogIn logIn = filledForm.get();
+            if (sphere().isLoggedIn()) {
+                return ok();
+            } else if (!sphere().login(logIn.email, logIn.password)) {
+                flash("error", "Invalid username or password.");
+                return badRequest(loginView.render(filledForm));
+            } else {
+                flash("success", "You are signed in.");
+                return redirect(routes.Application.home());
+            }
         }
-        // Case already logged in
-        LogIn logIn = form.get();
-        if (sphere().isLoggedIn()) {
-            return ok();
-        }
-        // Case invalid credentials
-        if (!sphere().login(logIn.email, logIn.password)) {
-            flash("error", "Invalid username or password");
-            return badRequest();
-        }
-        // Case valid log in
-        return ok();
     }
 
     public static Result logOut() {
