f69069771324bf9b8f5c498e7d29e3af280921d3
schleichardt <michael.schleichardt@commercetools.de>
2014-09-05 16:59:48 +0200

    Fix returnUrl bug.

--- app/controllers/ProductListController.java
+++ app/controllers/ProductListController.java
@@ -11,7 +11,6 @@ import services.*;
 
 import static utils.AsyncUtils.asPromise;
 
-@With(SaveContext.class)
 public class ProductListController extends BaseController {
 
     public ProductListController(CategoryService categoryService, ProductService productService,
@@ -19,6 +18,8 @@ public class ProductListController extends BaseController {
         super(categoryService, productService, cartService, customerService);
     }
 
+    //since this is the default endpoint if any other route does not match, it must not
+    //have the @With(SaveContext.class) annotation since this returns 404
     public F.Promise<Result> categoryProducts(String categorySlug, int page) {
         final Optional<ShopCategory> category = categoryService().getBySlug(locale(), categorySlug);
         if (category.isPresent()) {
@@ -30,6 +31,7 @@ public class ProductListController extends BaseController {
                             if (productList.isEmpty()) {
                                 flash("info-product-list", "No products found");
                             }
+                            session().put("returnUrl", request().uri());
                             return ok(showProductCategoryPage(data(), productList, category.get()));
                         }
                     });
@@ -38,6 +40,7 @@ public class ProductListController extends BaseController {
         }
     }
 
+    @With(SaveContext.class)
     public F.Promise<Result> searchProducts(int page) {
         final RequestParameters parameters = RequestParameters.of(queryString(), page);
         return productService().fetchSearchedProducts(locale(), parameters)
@@ -53,11 +56,13 @@ public class ProductListController extends BaseController {
 
     }
 
+    @With(SaveContext.class)
     static Content showProductCategoryPage(CommonDataBuilder dataBuilder, ProductList productList, ShopCategory category) {
         CommonData data = dataBuilder.withCategory(category).build();
         return views.html.productListView.render(data, productList);
     }
 
+    @With(SaveContext.class)
     static Content showProductSearchPage(CommonDataBuilder dataBuilder, ProductList productList) {
         return views.html.productListView.render(dataBuilder.build(), productList);
     }
