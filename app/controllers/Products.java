5180debed44020ed98e958fc0d7e72c2b5c60f47
schleichardt <michael.schleichardt@commercetools.de>
2014-09-08 10:12:38 +0200

    Show menu for product detail page.

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -9,16 +9,23 @@ import io.sphere.client.shop.model.Product;
 import io.sphere.client.shop.model.Variant;
 import play.mvc.Result;
 import play.mvc.With;
-import sphere.ShopController;
+import services.CartService;
+import services.CategoryService;
+import services.CustomerService;
+import services.ProductService;
 import views.html.productDetail;
 
 import java.util.Collections;
 
 
 @With(SaveContext.class)
-public class Products extends ShopController {
+public class Products extends BaseController {
 
-    public static Result select(String productSlug, int variantId) {
+    public Products(CategoryService categoryService, ProductService productService, CartService cartService, CustomerService customerService) {
+        super(categoryService, productService, cartService, customerService);
+    }
+
+    public Result select(String productSlug, int variantId) {
         Product product = sphere().products().bySlug(lang().toLocale(), productSlug).fetch().orNull();
         if (product == null) {
             return notFound("Product not found: " + productSlug);
@@ -27,7 +34,7 @@ public class Products extends ShopController {
     }
 
     //TODO this should not be indexed by search engines
-    public static Result selectById(String productId, int variantId) {
+    public Result selectById(String productId, int variantId) {
         Product product = sphere().products().byId(productId).fetch().orNull();
         if (product == null) {
             return notFound("Product not found: " + productId);
@@ -35,11 +42,11 @@ public class Products extends ShopController {
         return renderProduct(variantId, product);
     }
 
-    private static Result renderProduct(int variantId, Product product) {
+    private Result renderProduct(int variantId, Product product) {
         Variant variant = product.getVariants().byId(variantId).or(product.getMasterVariant());
         Category category = product.getCategories().get(0);
         FilterExpression categoryFilter = new FilterExpressions.Categories(Collections.singletonList(category));
         SearchResult<Product> searchResult = sphere().products().filter(lang().toLocale(), categoryFilter).fetch();
-        return ok(productDetail.render(product, variant, category, searchResult));
+        return ok(productDetail.render(data().build(), product, variant, category, searchResult));
     }
 }
